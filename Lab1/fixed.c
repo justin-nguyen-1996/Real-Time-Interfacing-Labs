a1a31c55a7dcb658507441ca16cba340c7865ffd
Justin Nguyen <2014justinnguyen@gmail.com>
2016-08-30 15:04:37 -0500

    program is much more modular now

--- Lab1/fixed.c
+++ Lab1/fixed.c
@@ -9,6 +9,39 @@
 #include "../h/fixed.h"
 #include "../h/ST7735.h" 
 
+void outDecToBuffer(int num, char out_buffer[], int NUM_OUTPUT_CHARS){
+	// Start writing at (NUM_OUTPUT_CHARS - 1) because we need room for the null terminator
+	int write_index = NUM_OUTPUT_CHARS - 1;
+	
+	// Digits in decimal are (digit + 0x30) in ASCII. (Example: '3' --> 0x33).
+	static int INT_TO_CHAR_OFFSET = 0x30;
+	
+	// Loop condition is (count > 1) because we need room for the decimal point
+	for (int count = NUM_OUTPUT_CHARS; count > 1; --count) {
+		out_buffer[write_index] = num % 10 + INT_TO_CHAR_OFFSET;
+		num /= 10;
+		write_index -= 1;
+	}
+}
+
+void formatBuffer(char out_buffer[], int DECIMAL_INDEX) {
+	// Shift the characters in the buffer to make room for the decimal point.
+	for (int i = 0; i < DECIMAL_INDEX; ++i) {
+		out_buffer[i] = out_buffer[i + 1];
+	}
+	
+	// Write in the decimal point.
+	out_buffer[DECIMAL_INDEX] = '.';
+	
+	// Replace extraneous leading zeroes with ' '
+	int first_valid_zero_index = DECIMAL_INDEX - 1;
+	for (int i = 0; i < first_valid_zero_index ; ++i) {
+		if (out_buffer[i] == '0') {
+			out_buffer[i] = ' ';
+		}
+	}
+}
+
 /* Input: signed 32-bit integer part of the fixed point number
  * Output: none
  * Resolution: 0.001
@@ -27,55 +60,31 @@
  */
 void ST7735_sDecOut3(int32_t num) {
 	// Take care of invalid input and negative numbers
-	int was_negative = 0;
+	int num_was_negative = 0;
 	if (num < -9999   ||   num > 9999) {
 		ST7735_OutString(" *.***\n");
 		return;
 	} else if (num < 0) {
 		num *= -1;
-		was_negative = 1;
+		num_was_negative = 1;
 	}
 	
-	// INT_TO_CHAR_OFFSET: Digits in decimal are (digit + 0x30) in ASCII (e.g. '3' --> 0x33).
 	// NUM_OUTPUT_CHARS:   As specified by the function, the number of chars to output is always 6.
-	// out_buffer:         There will be an output buffer containing 7 chars (6 values to output, 1 null character)
-	// count:              Count keeps track of how many chars have not been written to the buffer.
-	// 		               Decrement count until greater than two to save a spot for the decimal point and negative sign.
-	const int INT_TO_CHAR_OFFSET = 0x30;
+	// DECIMAL_INDEX:      Simply indicates where the decimal point will be written
+	// out_buffer:         Buffer the output --> call ST7735_OutString() once instead of ST7735_OutChar() multiple times
 	const int NUM_OUTPUT_CHARS = 6;
+	const int DECIMAL_INDEX = 2;
 	char out_buffer[NUM_OUTPUT_CHARS + 1];
-	int count = NUM_OUTPUT_CHARS;
-	int write_index = NUM_OUTPUT_CHARS - 1;
-	
-	// Append the null zero.
-	out_buffer[NUM_OUTPUT_CHARS] = 0;
 
-	// Write the chars to be displayed to a buffer.
-	// Write the LSB into the buffer but make sure to save two spots for the decimal point and negative sign
-	while (count > 2) {
-		out_buffer[write_index] = num % 10 + INT_TO_CHAR_OFFSET;
-		num /= 10;
-		count -= 1; write_index -= 1;
-	}
+	// Write the chars to be displayed to a buffer. Append the terminating null zero.
+	outDecToBuffer(num, out_buffer, NUM_OUTPUT_CHARS);
+	out_buffer[NUM_OUTPUT_CHARS] = 0;
 	
-	// Shift the characters in the buffer to make room for the decimal point.
-	int DECIMAL_INDEX = 2;
-	for (int i = 0; i < DECIMAL_INDEX; ++i) {
-		out_buffer[i] = out_buffer[i + 1];
-	}
+	// Format the buffer to include a decimal point and to remove extra leading zeroes.
+	formatBuffer(out_buffer, DECIMAL_INDEX);
 	
-	// Write the decimal point and potential negative sign in.
-	out_buffer[DECIMAL_INDEX] = '.';
-	if (was_negative) { out_buffer[0] = '-'; }
-	else { out_buffer[0] = num + INT_TO_CHAR_OFFSET; }
-	
-	// Replace extraneous leading zeroes with ' '
-	int first_valid_zero_index = DECIMAL_INDEX - 1;
-	for (int i = 0; i < first_valid_zero_index ; ++i) {
-		if (out_buffer[i] == '0') {
-			out_buffer[i] = ' ';
-		}
-	}
+	// Write in the potential negative sign.
+	if (num_was_negative) { out_buffer[0] = '-'; }
 	
 	// Write out the buffer.
 	ST7735_OutString(out_buffer);
@@ -103,17 +112,20 @@ void ST7735_sDecOut3(int32_t num) {
        256000	  "***.**"
  */
 void ST7735_uBinOut8(uint32_t num) {
+	// Take care of invalid input
 	if (num > 255999) {
 		ST7735_OutString("***.**\n");
 		return;
 	}
 	
-	// digits in decimal are (digit + 0x30) in ASCII (e.g. '3' --> 0x33)
-	const int INT_TO_CHAR_OFFSET = 0x30;
+	// NUM_OUTPUT_CHARS:   As specified by the function, the number of chars to output is always 6.
+	// DECIMAL_INDEX:      Simply indicates where the decimal point will be written
+	// out_buffer:         Buffer the output --> call ST7735_OutString() once instead of ST7735_OutChar() multiple times
 	const int ONE_OVER_RESOLUTION = 256;
 	const int NUM_OUTPUT_CHARS = 6;
 	const int DECIMAL_INDEX = 3;
 	const int NUM_DECIMAL_PLACES = NUM_OUTPUT_CHARS - DECIMAL_INDEX - 1;
+	char out_buffer[NUM_OUTPUT_CHARS + 1];
 	
 	// Find the number of times to shift num to make scaled_num
 	// Num * Resolution should equal (Num >> num_shifts)
@@ -136,39 +148,20 @@ void ST7735_uBinOut8(uint32_t num) {
 	if (scaled_num % 10 >= 5) { scaled_num = (scaled_num / 10) + 1; } 
 	else { scaled_num /= 10; }
 	
-	char out_buffer[NUM_OUTPUT_CHARS + 1];
-	int write_index = NUM_OUTPUT_CHARS - 1;
-	for (int count = NUM_OUTPUT_CHARS; count > 1; --count) {
-		out_buffer[write_index] = scaled_num % 10 + INT_TO_CHAR_OFFSET;
-		scaled_num /= 10;
-		write_index -= 1;
-	}
+	// Write the chars to be displayed to a buffer. Append the terminating null zero.
+	outDecToBuffer(scaled_num, out_buffer, NUM_OUTPUT_CHARS);
 	
-	// Shift the characters in the buffer to make room for the decimal point.
-	for (int i = 0; i < DECIMAL_INDEX; ++i) {
-		out_buffer[i] = out_buffer[i + 1];
-	}
+	// Format the buffer to include a decimal point and to remove extra leading zeroes.
+	formatBuffer(out_buffer, DECIMAL_INDEX);
 	
-	// Write the decimal point and append a null zero
-	out_buffer[DECIMAL_INDEX] = '.';
-	out_buffer[NUM_OUTPUT_CHARS] = 0;
-	
-	// Replace extraneous leading zeroes with ' '
-	int first_valid_zero_index = DECIMAL_INDEX - 1;
-	for (int i = 0; i < first_valid_zero_index ; ++i) {
-		if (out_buffer[i] == '0') {
-			out_buffer[i] = ' ';
-		}
-	}
-	
+	// Output to the LCD
 	ST7735_OutString(out_buffer);
-	
-	ST7735_OutChar('\n'); // output a new line
+	ST7735_OutChar('\n');
 }
 
 // This function will configure the plot and clear the drawing area
 void ST7735_XYplotInit() {
- 
+	
 }
 
 // Actually plot the X-Y scatter plot
