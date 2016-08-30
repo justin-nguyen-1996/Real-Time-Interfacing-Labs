9498ec94f37b0ec5758b742a60bab977980f5c9d
Justin Nguyen <2014justinnguyen@gmail.com>
2016-08-30 08:54:42 -0500

    DecOut() works, testing BinOut now

--- Lab1/fixed.c
+++ Lab1/fixed.c
@@ -26,7 +26,7 @@
 	  -12345    " *.***"
  */
 void ST7735_sDecOut3(int32_t num) {
-	// Take care of invalid input
+	// Take care of invalid input and negative numbers
 	int was_negative = 0;
 	if (num < -9999   ||   num > 9999) {
 		ST7735_OutString(" *.***\n");
@@ -40,7 +40,7 @@ void ST7735_sDecOut3(int32_t num) {
 	// NUM_OUTPUT_CHARS:   As specified by the function, the number of chars to output is always 6.
 	// out_buffer:         There will be an output buffer containing 7 chars (6 values to output, 1 null character)
 	// count:              Count keeps track of how many chars have been written to the buffer.
-	// 		               Decrement count until greater than one to save a spot for the decimal point.
+	// 		               Decrement count until greater than two to save a spot for the decimal point and negative sign.
 	static int INT_TO_CHAR_OFFSET = 0x30;
 	static int NUM_OUTPUT_CHARS = 6;
 	char out_buffer[NUM_OUTPUT_CHARS + 1];
@@ -52,23 +52,11 @@ void ST7735_sDecOut3(int32_t num) {
 
 	// Write the chars to be displayed to a buffer.
 	// Write the LSB into the buffer but make sure to save two spots for the decimal point and negative sign
-	// Write the LSB into the buffer until only one spot left for the decimal point.
 	while (count > 2) {
 		out_buffer[write_index] = num % 10 + INT_TO_CHAR_OFFSET;
 		num /= 10;
 		count -= 1; write_index -= 1;
 	}
-//	out_buffer[5] = 18 % 10 + INT_TO_CHAR_OFFSET;
-//	out_buffer[4] = '3';
-//	out_buffer[3] = '2';
-//	out_buffer[2] = '1';
-//	out_buffer[1] = '1';
-//	out_buffer[0] = '1';
-//	ST7735_OutString(out_buffer);
-//	ST7735_OutChar(out_buffer[5]);
-//	ST7735_OutChar(out_buffer[4]);
-//	ST7735_OutChar(out_buffer[3]);
-//	ST7735_OutChar(out_buffer[2]);
 	
 	// Shift the characters in the buffer to make room for the decimal point.
 	int DECIMAL_INDEX = 2;
@@ -79,7 +67,7 @@ void ST7735_sDecOut3(int32_t num) {
 	// Write the decimal point and potential negative sign in.
 	out_buffer[DECIMAL_INDEX] = '.';
 	if (was_negative) { out_buffer[0] = '-'; }
-	else { out_buffer[0] = num; }
+	else { out_buffer[0] = num + INT_TO_CHAR_OFFSET; }
 	
 	// Replace extraneous leading zeroes with ' '
 	int first_valid_zero_index = DECIMAL_INDEX - 1;
@@ -91,8 +79,6 @@ void ST7735_sDecOut3(int32_t num) {
 	
 	// Write out the buffer.
 	ST7735_OutString(out_buffer);
-//	char out_buffer2[6] = {3 + 0x30,'b','c','d','e','f'};
-//	ST7735_OutString(out_buffer2);
 	ST7735_OutChar('\n');
 }
 
@@ -119,26 +105,17 @@ void ST7735_sDecOut3(int32_t num) {
 void ST7735_uBinOut8(uint32_t num) {
 	if (num > 255999) {
 		ST7735_OutString("***.**");
-	} else if (num == 0) {
-		ST7735_OutString("0.00");
 	}
 	
+	int scaled_num = num * 100 >> 8;
+	
 	// digits in decimal are (digit + 0x30) in ASCII (e.g. '3' --> 0x33)
-	// the initial divisor will always be 1000
 	static int INT_TO_CHAR_OFFSET = 0x30;
-//	int num_shifts = 
+	static int ONE_OVER_RESOLUTION = 256;
 	
-	// output the most significant digit first, then the decimal point
-//	ST7735_OutChar(num / divisor + INT_TO_CHAR_OFFSET);
-//	num %= divisor;
-//	divisor /= 10;
-//	ST7735_OutChar('.');
-	
-	// output the rest of the digits
-	while (num > 0) {
-//			ST7735_OutChar(num / divisor + INT_TO_CHAR_OFFSET);
-//			num %= divisor;
-//			divisor /= 10;
+	while (scaled_num> 0) {
+		ST7735_OutChar(scaled_num % 10 + INT_TO_CHAR_OFFSET);
+		scaled_num /= 10;
 	}
 	
 	ST7735_OutChar('\n'); // output a new line
