54bcbc767b5ddb5019921db06849acce317984d5
U-LEMONHOPE\T <tmurdock66@gmail.com>
2016-09-21 12:38:07 -0500

    asdf

--- _Lab3_Skeleton/out.h
+++ _Lab3_Skeleton/out.h
@@ -2,6 +2,12 @@
 #define __outh
 
 #include <stdint.h>
+#define PF4  (*((volatile uint32_t *)0x40025040))
+#define PF3  (*((volatile uint32_t *)0x40025020))
+#define PF2  (*((volatile uint32_t *)0x40025010))
+#define PF1  (*((volatile uint32_t *)0x40025008))
+  
+#define PE1  (*((volatile uint32_t *)0x40024008))
 
 void out_Init(void);
 
