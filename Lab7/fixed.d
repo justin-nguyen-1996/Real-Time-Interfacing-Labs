1cc0b9c3bbcd184ff7c58a4f9334022ee6c3b443
Justin Nguyen <2014justinnguyen@gmail.com>
2016-10-18 23:46:41 -0500

    done with Lab 7 prep

---
+++ Lab7/fixed.d
@@ -0,0 +1,3 @@
+.\fixed.o: fixed.c
+.\fixed.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\fixed.o: ST7735.h
---
+++ Lab7/fixed.h
@@ -0,0 +1,85 @@
+// filename ******** fixed.h ************** 
+// Header file for Lab 1 
+// Trevor Murdock - ttm436
+// 8/30/2016
+#include <stdint.h>
+
+#ifndef _FIXEDH_
+#define _FIXEDH_
+
+/****************ST7735_sDecOut3***************
+ converts fixed point number to LCD
+ format signed 32-bit with resolution 0.001
+ range -9.999 to +9.999
+ Inputs:  signed 32-bit integer part of fixed-point number
+ Outputs: none
+ send exactly 6 characters to the LCD 
+Parameter LCD display
+ 12345    " *.***"
+  2345    " 2.345"  
+ -8100    "-8.100"
+  -102    "-0.102" 
+    31    " 0.031" 
+-12345    " *.***"
+ */ 
+void ST7735_sDecOut3(int32_t n);
+
+
+/**************ST7735_uBinOut8***************
+ unsigned 32-bit binary fixed-point with a resolution of 1/256. 
+ The full-scale range is from 0 to 999.99. 
+ If the integer part is larger than 256000, it signifies an error. 
+ The ST7735_uBinOut8 function takes an unsigned 32-bit integer part 
+ of the binary fixed-point number and outputs the fixed-point value on the LCD
+ Inputs:  unsigned 32-bit integer part of binary fixed-point number
+ Outputs: none
+ send exactly 6 characters to the LCD 
+Parameter LCD display
+     0	  "  0.00"
+     2	  "  0.01"
+    64	  "  0.25"
+   100	  "  0.39"
+   500	  "  1.95"
+   512	  "  2.00"
+  5000	  " 19.53"
+ 30000	  "117.19"
+255997	  "999.99"
+256000	  "***.**"
+*/
+void ST7735_uBinOut8(uint32_t n); 
+
+/**************ST7735_XYplotInit***************
+ Specify the X and Y axes for an x-y scatter plot
+ Draw the title and clear the plot area
+ Inputs:  title  ASCII string to label the plot, null-termination
+          minX   smallest X data value allowed, resolution= 0.001
+          maxX   largest X data value allowed, resolution= 0.001
+          minY   smallest Y data value allowed, resolution= 0.001
+          maxY   largest Y data value allowed, resolution= 0.001
+ Outputs: none
+ assumes minX < maxX, and miny < maxY
+*/
+void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY);
+
+/**************ST7735_XYplot***************
+ Plot an array of (x,y) data
+ Inputs:  num    number of data points in the two arrays
+          bufX   array of 32-bit fixed-point data, resolution= 0.001
+          bufY   array of 32-bit fixed-point data, resolution= 0.001
+ Outputs: none
+ assumes ST7735_XYplotInit has been previously called
+ neglect any points outside the minX maxY minY maxY bounds
+*/
+void ST7735_XYplot(uint32_t num, int32_t bufX[], int32_t bufY[]);
+void ST7735_Xcount_Yplot(uint32_t num, uint32_t bufY[]);
+
+/* Summary: Draws one line on the ST7735 color LCD
+ * Input:   (x1,y1) is the start point
+ *          (x2,y2) is the end point
+ *          color is 16-bits and be produced by ST7735_Color565() 
+ * Output: none
+ */
+void ST7735_Line(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2); 
+
+
+#endif
