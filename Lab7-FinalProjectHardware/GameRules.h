d17399ca992a90db66142ca00ceb1939f2d2d7d6
U-LEMONHOPE\T <tmurdock66@gmail.com>
2016-10-29 05:10:21 -0500

    cplusplusbitchbitch

--- Lab7-FinalProjectHardware/GameRules.h
+++ Lab7-FinalProjectHardware/GameRules.h
@@ -15,51 +15,95 @@ typedef enum {
   MISSILE
 } EntityType;
 
-typedef struct Loc {
-  uint8_t x;
-  uint8_t y;
-} Loc;
+//typedef struct Loc {
+//  uint8_t x;
+//  uint8_t y;
+//} Loc;
+//
+//typedef struct Vel {
+//  uint8_t speed;
+//  uint8_t dir;
+//} Vel;
 
-typedef struct Vel {
-  uint8_t speed;
-  uint8_t dir;
-} Vel;
+typedef struct Vector {
+	public:
+	uint16_t x;
+	uint16_t y;
 
-typedef struct Entity {
-  Vel vel;
-  Loc loc;
-  EntityType id;
-} Entity;
+	Vector operator+(const Vector & v)
+	{
+		Vector c;
+		c.x = x + v.x;
+		c.y = y + v.y;
+		return c;
+	}
 
-typedef struct Asteroid {
-  Vel vel;
-  Loc loc;
-  Entity id;
-} Asteroid;
+	Vector operator+=(const Vector & v)
+	{
+		x = x + v.x;
+		y = y + v.y;
+		return *this;
+	} 
+} Vector;
 
-typedef struct Ship {
-  Vel vel;
-  Loc loc;
-  Entity id;
-} Ship;
+class Spatial {
+	public:
+	Vector Position;
+	Vector Velocity;
+	Vector Acceleration;
+	
+	Spatial (Vector P, Vector V, Vector A) : Position(P), Velocity(V), Acceleration(A) {};
+	void update (void);
+};
+	
+//typedef struct Entity {
+//  Vel vel;
+//  Loc loc;
+//  EntityType id;
+//} Entity;
 
-// TODO: do we want a separate type of entity called "projectile"?
-typedef struct Laser {
-  Vel vel;
-  Loc loc;
-  Entity id;
-} Laser;
+class Asteroid : public Spatial {
+	public:
+	uint8_t radius;
+	Asteroid (Vector P, Vector V, Vector A, uint8_t r) : Spatial(P, V, A), radius(r) {}
+};
 
-typedef struct Missile {
-  Vel vel;
-  Loc loc;
-  Entity id;
-} Missile;
+class Ship : public Spatial {
+	uint8_t playerID;
+	uint16_t health;
+	uint16_t bullets;
+// other things like reload time remaining, rate of fire, special weapons, special abilities, etc.
+	Ship (uint8_t pID, uint16_t h, uint16_t b, Vector P, Vector V, Vector A) : 
+		Spatial(P, V, A), playerID(pID), health(h), bullets(b) {}
+	
+};
 
-uint8_t isCollision(Entity* a, Entity* b);
-void resolveCollision(Entity* a, Entity* b);
-void updateLoc(Entity* e);
-void updateVel(Entity* e);
-void updateState(Entity* e);
+// Base class for things that do damage
+// timer is for things that will dissapear or blow up after some delay
+class Shoot : public Spatial {
+	uint8_t damage;
+	uint16_t timer;
+	Shoot (Vector P, Vector V, Vector A, uint8_t d, uint16_t t) : Spatial(P, V, A), damage(d), timer(t) {}
+};
+
+//typedef struct Laser {
+//  Vel vel;
+//  Loc loc;
+//  Entity id;
+//} Laser;
+//
+//typedef struct Missile {
+//  Vel vel;
+//  Loc loc;
+//  Entity id;
+//} Missile;
+
+//still reading about collisions in a quadtree
+
+//uint8_t isCollision(Entity* a, Entity* b);
+//void resolveCollision(Entity* a, Entity* b);
+//void updateLoc(Entity* e);
+//void updateVel(Entity* e);
+//void updateState(Entity* e);
 
 #endif
--- Lab7-FinalProjectHardware/adc.d
+++ Lab7-FinalProjectHardware/adc.d
@@ -1,6 +1,6 @@
 .\adc.o: ADC.c
 .\adc.o: ../inc/tm4c123gh6pm.h
 .\adc.o: ADC.h
-.\adc.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\adc.o: C:\Program Files\Keil\ARM\ARMCC\bin\..\include\stdint.h
 .\adc.o: Accel.h
 .\adc.o: Thumbstick.h
